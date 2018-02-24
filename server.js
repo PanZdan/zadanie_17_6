f26f37b4b2ef40233f37a52976e54b4aafbfb7d9
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-02-24 21:52:11 +0100

    Added first versions

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "pug");//kreator widoków
+app.set("views", "./views");//katalog widoków
+
+app.get("/", function(req, res) {
+  res.render("index");
+});
+
+app.get("/auth/google", function(req, res) {
+  
+  res.render("loginsite");
+});
+
+app.listen(3000);
+app.use(function(req, res, next) {
+  res.status(404).send("Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!");
+});
